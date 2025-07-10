6ff91e4cd7bff4542cc392fd9ebe57640b6f404e
om07github <mahalleom794@gmail.com>
2025-07-10 09:05:02 +0530

    map-Commit-2

--- client/src/pages/contact.jsx
+++ client/src/pages/contact.jsx
@@ -1,29 +1,37 @@
 import React from "react";
 import Navbar from "../components/navbar";
+import Footer from "../components/footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
+import 'leaflet-defaulticon-compatibility';
 import './home.css';
-import Footer from "../components/footer";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function Contact() {
     return (
         <div>
             <Navbar centerText="Contact Us" backgroundImage="https://themewagon.github.io/montana/img/banner/bradcam3.png" />
 
+            {/* Leaflet Map */}
             <div className="d-flex justify-content-center">
-                <div className="map-container w-100">
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3783.254536117537!2d73.84474017481555!3d18.514507869160882!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c07a49b95d07%3A0xf769bdc70171f2c0!2sPune%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1722947983456!5m2!1sen!2sin"
-                        width="100%"
-                        height="300"
-                        style={{ border: 0 }}
-                        allowFullScreen=""
-                        loading="lazy"
-                        referrerPolicy="no-referrer-when-downgrade"
+                <MapContainer
+                    center={[18.5204, 73.8567]} // Pune coordinates
+                    zoom={13}
+                    scrollWheelZoom={false}
+                    style={{ height: "300px", width: "90%" }}
+                >
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
                     />
-                </div>
+                    <Marker position={[18.5204, 73.8567]}>
+                        <Popup>Luxury Stay Hotel, Pune, Maharashtra</Popup>
+                    </Marker>
+                </MapContainer>
             </div>
 
+            {/* Contact Form */}
             <div className="container">
                 <div className="row">
                     <div className="col-md-2 my-5 mt-5 mb-5"></div>
